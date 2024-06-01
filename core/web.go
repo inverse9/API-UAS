144c667c6f8fb45515ed469b86a8e0579227535d
inverse9 <permanawahyu97@gmail.com>
2024-06-01 21:47:05 +0800

    adding cors rule, to prevent cors err

--- core/web.go
+++ core/web.go
@@ -1,9 +1,24 @@
 package core
 
-import "github.com/gin-gonic/gin"
+import (
+	"time"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+)
 
 func NewWeb() *gin.Engine {
 	web := gin.Default()
-
+	web.Use(cors.New(cors.Config{
+        AllowOrigins:     []string{"*"},
+        AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
+        AllowHeaders:     []string{"Origin", "Content-Type", "Accept"},
+        ExposeHeaders:    []string{"Content-Length"},
+        AllowCredentials: true,
+        AllowOriginFunc: func(origin string) bool {
+            return true
+        },
+        MaxAge: 12 * time.Hour,
+    }))
 	return web
 }
--- service/entities/transaction.go
+++ service/entities/transaction.go
@@ -6,7 +6,7 @@ type Transaction struct {
 	ID          *int    `gorm:"column:id" json:"id"`
 	Address     *string `gorm:"column:address" json:"address"`
 	CreatedAt   *string `gorm:"column:created_at" json:"created_at"`
-	ProductId   *int    `gorm:"column:product_id" json:"product_id"`
+	ProductId   *string `gorm:"column:product_id" json:"product_id"`
 	ProductName *string `gorm:"column:productName" json:"productName"`
 	UserId      *int    `gorm:"column:user_id" json:"user_id"`
 	UserName    *string `gorm:"column:userName" json:"userName"`
@@ -15,7 +15,7 @@ type Transaction struct {
 type TransactionInsert struct {
 	ID        *int    `gorm:"column:id" json:"id"`
 	Address   *string `gorm:"column:address" json:"address"`
-	ProductId *int    `gorm:"column:product_id" json:"product_id"`
+	ProductId *string `gorm:"column:product_id" json:"product_id"`
 	UserId    *int    `gorm:"column:user_id" json:"user_id"`
 }
 
